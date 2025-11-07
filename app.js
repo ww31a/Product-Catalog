afe4d554ca7da32ddbe3a80ee9df42a6dff8b2e9
waqasa1 <waqasanwar1308@gmail.com>
2025-11-07 18:33:13 +0500

    json token

--- app.js
+++ app.js
@@ -6,30 +6,39 @@ import cors from 'cors'
 import productrouter from './routes/products.routes.js';
 import authrouter from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 
 await connectDB();
 
 const app = express()
 const PORT = 5000
+const _dirname = path.resolve();
+console.log(_dirname)
 
 app.use(cookieParser())
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cors({
-    origin: true,
-    credentials: true,
-}));
+app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: [
+            "http://localhost:5173",
+            "http://192.168.18.22:5173", 
+        ],
+        credentials: true,
+    }));
+}
 
 app.use('/api/products', productrouter)
 app.use('/api/admin/auth', authrouter)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../frontend/dist")))
+    app.use('*', (req, res) => {
+        res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-
-app.listen(PORT,"0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Product Catalog backend running on http://0.0.0.0:${PORT}`)
 })
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import { adminLogin, adminLogout } from "../controllers/auth.controller.js";
+import { adminLogin } from "../controllers/auth.controller.js";
 import limiter from "../middlewares/ratelimit.js";
-import { checkToken } from "../controllers/checkadmin.controller.js";
 const authrouter = Router();
 
 authrouter.post('/login',limiter, adminLogin)
-authrouter.post('/logout', adminLogout)
 
-authrouter.get("/me",checkToken);
 
 export default authrouter;
